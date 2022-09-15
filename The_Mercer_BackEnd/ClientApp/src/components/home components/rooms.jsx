4267db15ab8e8a4eeb66735823d8e6b2ce541fd5
Isac Gansmoe <isac@gansmoe.se>
2022-09-15 14:04:39 +0200

    FE: Rebuilt rooms to a class, added readmore with clickable arrow

--- The_Mercer_BackEnd/ClientApp/src/components/home components/rooms.jsx
+++ The_Mercer_BackEnd/ClientApp/src/components/home components/rooms.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 
 
-const Rooms = ({ room }) => {
+export default class Rooms extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rooms: [],
+            showmore: false
+        };
+    }
+
+    eventHandler = (event) => {
+        if (this.state.showmore === false) {
+            this.setState({showmore: true});
+        } else {
+            this.setState({showmore: false});
+        }
+        
+        event.target.classList.toggle("up");
+    }
 
 
+    render() {
     return (
         <div className="rooms">
-            <h5>{room.roomName}</h5>
-            <button>Återställ</button>
-            <i className="arrow down"></i>
+            <h5>{this.props.room.roomName}</h5>
+            <button className="alarmBtn">Återställ</button>
+            <i className="arrow down" onClick={this.eventHandler}></i>
+            {this.state.showmore === true ? <div className="room-info"> <p>Temperatur: 23°C</p> <p>Luftfuktighet: 62%</p> <p>Uppdaterad: 2022-09-15</p> </div> : null}
         </div>
     );
+    }
 }
 
-export default Rooms;
