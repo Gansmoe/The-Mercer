51bd167cbf67e65b24dbcd99604ae00624cbf3a5
Henrik Almqvist <almqvist.henric@gmail.com>
2022-09-21 13:59:56 +0200

    FE: Changed some CSS

--- The_Mercer_BackEnd/ClientApp/src/components/header/Header.jsx
+++ The_Mercer_BackEnd/ClientApp/src/components/header/Header.jsx
@@ -1,13 +1,8 @@
-
 import { NavLink,Button, Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
-
 import "../../custom.scss";
-
 import { useDispatch } from "react-redux";
 import {  REMOVE_ACTIVE_USER } from "../../redux/slice/authSlice";
-
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn, selectName } from "../../redux/slice/authSlice";
 
@@ -40,7 +35,7 @@ function Header() {
   return (
     <Navbar bg="light" expand="lg">
       <Container fluid>
-        <Navbar.Brand href="#">The Mercer</Navbar.Brand>
+        <Navbar.Brand href="#"><h1 className="h1-brand-nav">The Mercer</h1></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
