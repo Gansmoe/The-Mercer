b6d4cc6c3bd2ff08631c1554d39c2e2a8d46dca3
Isac Gansmoe <isac@gansmoe.se>
2022-09-22 16:20:58 +0200

    Minor changes. Makes both alarms trigger.

--- The_Mercer_BackEnd/ClientApp/src/Helpers/Calculation.js
+++ The_Mercer_BackEnd/ClientApp/src/Helpers/Calculation.js
@@ -11,26 +11,41 @@ export const MatchValues = (telemetry, rooms, range) => {
                 rooms[i].tempValue = Math.round(telemetry[0].value);
                 rooms[i].time = new Date(telemetry[0].time * 1000).toLocaleTimeString();
                 
-                for (let x = 0; x < range.length; x++) {
-                    if (range[x].id === rooms[i].tempDevice.toLowerCase()) {
-                        if (rooms[i].tempValue >= range.maxValue) {
+                for (let y = 0; y < range.length; y++) {
+
+                    if (range[y].id === rooms[i].tempDevice.toLowerCase()) {
+
+                        if (rooms[i].tempValue >= range[y].maxValue) {
+                            console.log("från in i if alarm satsen");
+
                             rooms[i].tempAlarm = true;
-                        } else if (rooms[i].tempValue <= range.minValue) {
+
+                        } else if (rooms[i].tempValue <= range[y].minValue) {
+
                             rooms[i].tempAlarm = true;
+
                         }
                     }
                 }
                 return rooms;
             }
             else if (telemetry[0].deviceId === rooms[i].humidDevice) {
+
                 rooms[i].humidValue = Math.round(telemetry[0].value);
                 rooms[i].time = new Date(telemetry[0].time * 1000).toLocaleTimeString();
+
                 for (let y = 0; y < range.length; y++) {
+
                     if (range[y].id === rooms[i].humidDevice.toLowerCase()) {
+
                         if (rooms[i].humidValue >= range[y].maxValue) {
+
                             rooms[i].humidAlarm = true;
+
                         } else if (rooms[i].humidValue <= range[y].minValue) {
+
                             rooms[i].humidAlarm = true;
+
                         }
                     }
 
@@ -39,8 +54,6 @@ export const MatchValues = (telemetry, rooms, range) => {
             }
         }
     }
-
-
 
     return rooms;
 }
