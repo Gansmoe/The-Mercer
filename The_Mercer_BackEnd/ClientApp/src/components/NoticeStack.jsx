ff52063f0d71fab43caecd567b43e64434b3ceda
Christian Yngvesson <chrille_y@hotmail.com>
2022-09-25 10:35:04 +0200

    FE: Removed testNotice from noticeStack

--- The_Mercer_BackEnd/ClientApp/src/components/NoticeStack.jsx
+++ The_Mercer_BackEnd/ClientApp/src/components/NoticeStack.jsx
@@ -2,16 +2,6 @@ import Notice from "./Notice"
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
-
-const testNotice = [{
-    msg: 'There is yoghurt in the swimming pool.',
-    type: 'info',
-    callback: (id) => {
-        const target = document.getElementById('3'); // id should be passed here in normal cases
-        window.scrollTo(0, target.offsetTop);
-    }
-}];
-
 const NoticeStack = ({ list }) => {
     const [notices, setNotices] = useState([]);
     const getNoticeList = useSelector((state) => state.noticeStack.list);
@@ -19,9 +9,6 @@ const NoticeStack = ({ list }) => {
     useEffect(() => {
         setNotices(getNoticeList);
     }, [getNoticeList])
-
-
-
 
     // Some experimental stuff, move along...
     const addNotice = (msg, type, callback) => {
@@ -33,6 +20,7 @@ const NoticeStack = ({ list }) => {
         setNotices(notices.push(newNotice));
     }
     //..........................................
+    
     return (
         <div className="notice-list">
             {notices.length > 0 ?
